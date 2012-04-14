375c86102fae1c069127b7a1c9a3197df34680c8
Anders Steinlein <anders@e5r.no>
2012-04-14 02:48:40 +0200

    Add simple examples

---
+++ example/example.php
@@ -0,0 +1,35 @@
+<?php
+// new Api("...", "...", false, true) to enable debug output
+$sendgrid = new SendGrid\Api("my_username", "my_password");
+
+// Initialize new user without actually creating it
+$user = new SendGrid\SubUser("wanted_username", "password", "email@example.com",
+		"email.domain.example.com"); // Many optional arguments available
+
+// Add the sub user to your account
+$sendgrid->addSubUser($user);
+
+// Assign an IP to the user
+$user->assignIps(array("1.2.3.4"));
+
+// Enable and configure an app
+$user->enableApp("eventnotify", true, array(
+		'processed' => false,
+		'dropped' => true,
+		'deferred' => false,
+		'delivered' => false,
+		'bounce' => true,
+		'click' => false,
+		'open' => false,
+		'unsubscribe' => false,
+		'spamreport' => true,
+		'url' => 'http://example.com/url-to-event-receiver'));
+
+// Instead of creating a user, we can retrieve one
+$user = $sendgrid->getSubUser("another_sub_username");
+
+// Get a list of available apps for the user
+$apps = $user->getApps();
+
+// Get current settings for a specific app
+$settings = $user->getAppSettings("eventnotify");
